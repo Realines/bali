30d764bf90acb7088c9bc63de895f19f70dc0828
Alexey Bobrov <bobrov.bobrov-alex@yandex.ru>
2022-05-05 15:05:01 +0700

    Изменил настройки сайта

--- bali/urls.py
+++ bali/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.conf.urls.static import static
 from django.conf import settings
 from django.contrib import admin
@@ -31,4 +32,5 @@ urlpatterns = [
 
 # Генерирует маршрут до директории с медиафайлами.
 # Работает только в режиме отладки.
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
